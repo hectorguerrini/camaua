a07caca1226fc6219effe8d32cf12db2e8c196eb
HectorGuerrini <hectorumaga@hotmail.com>
2019-04-01 17:08:52 -0300

    valorfinal

--- src/app/pages/vendas/vendas.component.ts
+++ src/app/pages/vendas/vendas.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-vendas',
-  templateUrl: './vendas.component.html',
-  styleUrls: ['./vendas.component.scss']
+  selector: "app-vendas",
+  templateUrl: "./vendas.component.html",
+  styleUrls: ["./vendas.component.scss"]
 })
 export class VendasComponent implements OnInit {
-
-  tipoVenda: string = 'aluno';
+  tipoVenda: string = "aluno";
   RA: string;
   CPF: string;
   sexo: string;
@@ -15,15 +14,34 @@ export class VendasComponent implements OnInit {
   alimento: number;
   combo: number;
   numeroIngresso: string;
-  constructor() { }
 
-  ngOnInit() {
+  valores = [
+    { tipo: "aluno", lote: 1, valor: 55 },
+    { tipo: "aluno", lote: 2, valor: 60 },
+    { tipo: "aluno", lote: 3, valor: 65 },
+    { tipo: "convidado", lote: 1, valor: 70 },
+    { tipo: "convidado", lote: 2, valor: 80 },
+    { tipo: "convidado", lote: 3, valor: 90 }
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+
+  valorFinal(): number {
+    const valor = this.valores.find(
+      el => el.tipo === this.tipoVenda && el.lote === this.lote
+    );
+    let result = 0;
+    if (valor) {
+      result = valor.valor;
+      if (this.alimento) {
+        result -= 5;
+      }
+      if (this.combo) {
+        result += 12;
+      }
+    }
+
+    return result;
   }
-
-  valorFinal(): number{
-
-
-    return 0;
-  }
-
 }
